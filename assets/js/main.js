1aed739b4d20d397d78b230596f9e88bd9507c74
Sean Girard <seangirard@yahoo.com>
2014-05-27 12:25:30 -0700

    login

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,9 +41,27 @@ var splApp = {
 
 	init: function() {
 
-		var barcode = localStorage.getItem('barcode');
-		alert(barcode);
+		_self = this;
 
+		//localStorage.removeItem('splAccount');
+		//localStorage.removeItem('splAccounts');
+		
+		var splAccount = localStorage.getItem('splAccount');
+		var splAccounts = localStorage.getItem('splAccounts');
+
+		if ( null !== splAccounts ) {
+			splAccounts = JSON.parse(splAccounts);
+		}
+
+		this.splAccount = splAccount;
+		this.splAccounts = splAccounts;
+
+		this.initEvents();
+		this.showProfile();
+			
+	}, // init()
+
+	initEvents: function() {
 		$('body').on('submit', '#spl-login', function(e) {
 			e.preventDefault();
 
@@ -54,35 +72,96 @@ var splApp = {
 			var params = { barcode: barcode
 										,pin: pin };
 			
-
-			console.log(params);
-
-			$.ajax({ 
-        url: 'http://api.spokanelibrary.org/v2/hzws/borrower'
-        ,dataType: 'jsonp'
-        ,data: { params: params } 
-      })
-      .done(function(obj) {
-      	if ( obj.userID ) {
-      		localStorage.setItem('barcode',barcode);
-      		localStorage.setItem('pin',pin);
-      	}
-        console.log(obj);
-      })
-      .fail(function() { 
-      })
-      .always(function() {  
-      });
-		});
+			_self.addAccount(params);
 		
-		var data = {my:'data'};
+		});
+
+		$('body').on('click', '.spl-choose-account', function(e) {
+			e.preventDefault();
+
+			var id = $(this).data('spl-account');
+			_self.splAccount = id;
+
+			_self.saveAccounts();
+			_self.showProfile();
+		
+		});
+
+		$('body').on('click', '.spl-remove-account', function(e) {
+			e.preventDefault();
+
+			var id = $(this).data('spl-account');
+			_self.splAccount = null;
+			delete _self.splAccounts[id];
+
+			_self.saveAccounts();
+			_self.showProfile();
+		
+		});
+
+		$('body').on('click', '.prevent-default', function(e) {
+			e.preventDefault();
+		});
+
+	}, // initEvents
+
+	showProfile: function() {
+
+		//console.log(this.splAccount);
+		//console.log(this.splAccounts);
+
+		var accounts = this.splAccounts;
+		if ( null !== this.splAccount ) {
+			var account = this.splAccounts[this.splAccount];
+		}
+
+		var indexed = [];
+		for ( x in accounts ) {
+			indexed.push(accounts[x]);	
+		}
 
 		$user = $('#spl-user');
     tmpl = Handlebars.compile( $("#spl-user-tmpl").html() );
-    $user.html( tmpl({user:data}) );
+    $user.html( tmpl({ account: account, accounts:indexed }) );
 
+	}, // showProfile
+	
+	addAccount: function(params) {
 
+		$.ajax({ 
+      url: 'http://api.spokanelibrary.org/v2/hzws/borrower'
+      ,dataType: 'jsonp'
+      ,data: { params: params } 
+    })
+    .done(function(obj) {
+    	if ( obj.userID ) {
+    		
+				if ( null === _self.splAccounts ) {
+    			_self.splAccounts = {};
+    		}
+	      _self.splAccounts[obj.userID] = { barcode:params.barcode
+	      																, pin:params.pin
+	      																, id: obj.userID
+	      																, name:obj.accountInfo.name
+	      																, first:obj.horizonInfo.first_name };
+	      _self.splAccount = obj.userID;
 
-	}
+	      _self.saveAccounts();
+	  		_self.showProfile();
+    	}
+    })
+    .fail(function() { 
+    })
+    .always(function() {  
+    });
+
+	}, // addAccount
+
+	saveAccounts: function() {
+
+		localStorage.setItem('splAccounts',JSON.stringify(_self.splAccounts));
+	  localStorage.setItem('splAccount',_self.splAccount);
+
+	} // saveAccounts
 
 } // splApp()
